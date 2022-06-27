a0be14504c91a91f8745b0b909c72e4cce0b086b
Jose Motta <josemotta@bampli.com>
2022-06-27 20:18:50 -0300

    fix webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,13 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({template: 'src/assets/index.html'}),
+    // new Webpack.EnvironmentPlugin({
+    //   'NEO4J_URI': 'bolt+s://d8074969b9e77ba9985b14aaa44dd925.neo4jsandbox.com:7687',
+    //   'NEO4J_DATABASE': 'movies',
+    //   'NEO4J_USER': 'neo4j',
+    //   'NEO4J_PASSWORD': 'act-counts-sequences',
+    //   'NEO4J_VERSION': ''
+    // })
     new Webpack.EnvironmentPlugin({
       'NEO4J_URI': 'neo4j+s://demo.neo4jlabs.com',
       'NEO4J_DATABASE': 'movies',
